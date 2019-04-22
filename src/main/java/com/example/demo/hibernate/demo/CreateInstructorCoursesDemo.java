d332994aae3f702237d8ff8013b4bc805202053f
Ahmed Tarek <ahmed-tarek94@hotmail.com>
2019-04-22 12:50:01 +0200

    Manipulating objects

---
+++ src/main/java/com/example/demo/hibernate/demo/CreateInstructorCoursesDemo.java
@@ -0,0 +1,43 @@
+package com.example.demo.hibernate.demo;
+
+import com.example.demo.hibernate.Course;
+import com.example.demo.hibernate.InstructiorDetail;
+import com.example.demo.hibernate.Instructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+
+public class CreateInstructorCoursesDemo {
+
+    public static void main(String [] args) {
+        SessionFactory factory = new Configuration().configure()
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructiorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+
+
+
+        try {
+
+            session.beginTransaction();
+
+            Instructor instructor = (Instructor) session.get(Instructor.class, 2);
+
+            List<Course> courses = instructor.getCourses();
+
+            System.out.println(courses);
+
+            session.getTransaction().commit();;
+
+        } finally {
+            factory.close();
+
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/hibernate/demo/TestObjectManipulation.java
@@ -0,0 +1,41 @@
+package com.example.demo.hibernate.demo;
+
+import com.example.demo.hibernate.Course;
+import com.example.demo.hibernate.InstructiorDetail;
+import com.example.demo.hibernate.Instructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class TestObjectManipulation {
+
+    public static void main(String [] args) {
+        SessionFactory factory = new Configuration().configure()
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructiorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+
+
+
+        try {
+
+            session.beginTransaction();
+
+            Instructor instructor = (Instructor) session.get(Instructor.class, 2);
+
+           instructor.setEmail("Abo8azambal@hotmail.com");
+
+            session.getTransaction().commit();
+            System.out.println(instructor);
+
+        } finally {
+            factory.close();
+
+        }
+    }
+}
